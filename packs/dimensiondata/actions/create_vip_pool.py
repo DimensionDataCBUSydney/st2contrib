e0fcb80e0cee6f3c42416a96678b4fc9fab78b13
kumar chockalingam <kumarappanc@gmail.com>
2016-05-23 10:50:48 +1000

    create pool with monitors and vip with persistence profile

---
+++ packs/dimensiondata/actions/create_vip_pool.py
@@ -0,0 +1,28 @@
+from lib.actions import BaseAction
+
+__all__ = [
+    'CreatePoolAction'
+]
+
+
+class CreatePoolAction(BaseAction):
+
+    def run(self, region, network_domain_id, name, balancer_method, ex_description, health_monitors,
+            service_down_action, slow_ramp_time):
+        driver = self._get_lb_driver(region)
+        driver.network_domain_id = network_domain_id
+        health_monitors_list = driver.ex_get_default_health_monitors(network_domain_id)
+        monitors = list()
+        for monitor in health_monitors.split(","):
+            for available_monitor in health_monitors_list:
+                if available_monitor.name.lower() == ("ccdefault." + monitor.lower()):
+                    monitors.append(available_monitor)
+        record = driver.ex_create_pool(network_domain_id = network_domain_id,
+                                       name = name,
+                                       balancer_method = balancer_method,
+                                       ex_description = ex_description,
+                                       health_monitors=monitors,
+                                       service_down_action=service_down_action,
+                                       slow_ramp_time=slow_ramp_time
+                                      )
+        return self.resultsets.formatter(record)
--- packs/dimensiondata/actions/create_virtual_listener.py
+++ packs/dimensiondata/actions/create_virtual_listener.py
@@ -11,19 +11,25 @@ class CreateVirtualListenerAction(BaseAction):
 
     def run(self, region, network_domain_id, name, port, ex_description, protocol,
             pool_id, connection_limit, connection_rate_limit, source_port_preservation,
-            listener_ip_address, persistence_profile_id=None, fallback_persistence_profile_id=None,
+            listener_ip_address, persistence_profile=None, fallback_persistence_profile=None,
             irule_id=None):
         driver = self._get_lb_driver(region)
         driver.network_domain_id = network_domain_id
         pool = driver.ex_get_pool(pool_id)
+        available_persistence_profiles = driver.ex_get_default_persistence_profiles(network_domain_id)
+        for profile in available_persistence_profiles:
+            if profile.name == ("ccdefault." + persistence_profile):
+                persistence_profiles = profile
+            if profile.name == ("ccdefault." + fallback_persistence_profile):
+                fallback_persistence_profiles = profile
         record = driver.ex_create_virtual_listener(network_domain_id=network_domain_id,
                                                    name=name,
                                                    ex_description=ex_description,
                                                    port=port,
                                                    pool=pool,
                                                    listener_ip_address= listener_ip_address,
-                                                   persistence_profile=None,
-                                                   fallback_persistence_profile=None,
+                                                   persistence_profile=persistence_profiles,
+                                                   fallback_persistence_profile=fallback_persistence_profiles,
                                                    irule=None,
                                                    protocol=protocol,
                                                    connection_limit=connection_limit,
